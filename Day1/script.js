111e5988a28b7457a6953577db851b4406124ddc
ajit <ajeets@1028.com>
2025-04-17 11:41:27 +0530

    Initial Commit

---
+++ Day1/script.js
@@ -0,0 +1,38 @@
+/*
+Host: Any device(usually a computer) connected to a network. It can send or receive data.Every device with an IP Address
+(like your phone,tab,computer is a host).
+
+Server: A special type of host — it's a computer designed to provide services or resources (like websites, files, or data) to other hosts called clients.
+
+Server is also a computer.
+
+A browser (short for web browser) is a software application used to access and view websites on the internet.
+Popular browsers include Google Chrome, Mozilla Firefox, Safari, and Microsoft Edge.
+They retrieve content from web servers and display it to users, handling HTML, CSS, JavaScript, and more.
+
+Browser contains Address Bar where you search for url,Tabs like you open multiple webpage, Main content area display of a web page,back/forward buttons. 
+
+JavaScript is everywhere.
+
+Rendering is the process to show data on your UI that is coming as a responce from the server in the form of html and css.
+
+Client - Server Architecture:-> Let's understand these concept with the help of a simple example.Everyone here have 
+                                 use swiggy app. You type swiggy in your browser for ordering food. Then what happend 
+                                 a request is send from your computer(client) to the swiggy server that is kept in other 
+                                 place it receives the request and give html or css or html+css files as a resoponce
+                                 for your request that render on your UI.
+                                 Let's suppose you are thinking to order pizza for you then at that time you friend come and tell you
+                                 to order for him also then you click on add(+) button for two pizza. Then what you think will the  request
+                                 will also go to the server for when you click add(+) buttons or its handle by browser only?
+                                 Let me ans you its handle on the browser by itself using a famous beautiful programming language know as 
+                                 JAVASCRIPT. JavaScript is everywhere. 
+                                 This phenomenon from getting sending request and getting responce is known as client server architecture.
+                                 
+
+                            
+
+Every browser has its own javascript engine. Chrome has V8,Mozilla has spidermonkey like this Safari has its own etc.
+You can see gthe code using inspect buttons on your browser it will show code.
+
+
+*/
