01c36930935b5f1219d4ca9cdcb275017c4d3454
ajit <ajeets@1028.com>
2025-04-18 12:01:57 +0530

    day4

---
+++ Day4_DataTypes/script4.js
@@ -0,0 +1,12 @@
+/*
+    RAM store data inside it on the temporary basis.
+    you have haeard of GB(10^9 bytes) that is giga bytes.
+    ex: 2GB -> 2 * 10^9 bytes
+        1byte -> 8 bits
+        RAM is collections of bytes and byte is a collection of bits. bit is made up of  transistor(npm and npn) either 1 or 0.
+        that's why RAM contains only 1 and 0. 
+
+
+   Datatypes: Datatype is a convertor that converts real world data(which is not in the binary ie. 12, 1.22, "Ajs", Yes,movie,song etc) into the binary
+             so that it can store into the RAM.
+ */
